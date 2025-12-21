1f05746e3516d219db1da8cdfe63357c073f0c28
RoseTechWorks <Naomiobebeduo@gmail.com>
2025-12-20 23:06:07 -0600

    Update VPC and subnet configurations for EKS cluster

--- kubernetes/eks-cluster.tf
+++ kubernetes/eks-cluster.tf
@@ -22,9 +22,9 @@ module "eks" {
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
-  vpc_id                   = module.my-cluster-vpc.vpc_id
-  subnet_ids               = module.my-cluster-vpc.private_subnets
-  control_plane_subnet_ids = module.my-cluster-vpc.private_subnets
+  vpc_id                   = module.MongoDB-cluster-vpc.vpc_id
+  subnet_ids               = module.MongoDB-cluster-vpc.private_subnets
+  control_plane_subnet_ids = module.MongoDB-cluster-vpc.private_subnets
 
   # EKS Managed Node Group(s)
   eks_managed_node_groups = {
